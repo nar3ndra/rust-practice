9f231b8a6bf83045316114f7fa7f160e85d69d8d
nar3ndra <narendrameenuga321@gmail.com>
2024-08-03 14:26:41 -0400

    first

---
+++ hello_cargo/target/debug/deps/hello_cargo-148ddd3334c797e9.d
@@ -0,0 +1,5 @@
+C:\Users\nariw\github\rust-practice\hello_cargo\target\debug\deps\libhello_cargo-148ddd3334c797e9.rmeta: src/main.rs
+
+C:\Users\nariw\github\rust-practice\hello_cargo\target\debug\deps\hello_cargo-148ddd3334c797e9.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\nariw\github\rust-practice\hello_cargo\target\debug\hello_cargo.exe: C:\Users\nariw\github\rust-practice\hello_cargo\src\main.rs
